0ae61ce352a8903db9bed949f5ed51a104fd95d6
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-06-22 05:43:48 +0000

    use local dir

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf CMakeFiles/ CMakeCache.txt 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++-18 -DCMAKE_C_COMPILER=clang-18
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++-18 -DCMAKE_C_COMPILER=clang-18 .
 make -j 2
